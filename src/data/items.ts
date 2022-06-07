9a098fedb8fc06d49cf6131fe3d7c33af82eae0d
RaphaelMardine <rafamardine@hotmail.com>
2022-06-06 21:14:35 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15), category: 'food', title: 'MCDonalds', value: 32.12 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'BurgerKing', value: 17.12 },
+    { date: new Date(2021, 9, 15), category: 'rent', title: 'Aluguel apartamento', value: 1200 },
+    { date: new Date(2021, 9, 15), category: 'salary', title: 'Salário B2B Reservas', value: 3000 },
+]
